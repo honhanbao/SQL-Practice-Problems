5a0708e16fb9a25097cc979cc1f524a605ad8d67
Bao Ho <nhanbaoho@gmail.com>
2023-06-30 22:58:44 +1000

    Finish part 2 intermediate

--- Intermediate Problems 20 - 31.sql
+++ Intermediate Problems 20 - 31.sql
@@ -1,7 +1,14 @@
--- 20. Categories, and the total products in each category
--- For this problem, we’d like to see the total number of products in each category. 
--- Sort the results by the total number of products, in descending order.
-	--
+
+-- Intermediate Problems 20-31
+
+-- Note: some questions are modified, marked as "- modified"
+
+/*
+20. Categories, and the total products in each category
+For this problem, we’d like to see the total number of products in each category. 
+Sort the results by the total number of products, in descending order.
+*/	
+    --
 	Select 
 		c.CategoryName			 "Category Name"
 		,count(p.Productname)	 "Total Producs"
@@ -21,8 +28,10 @@
 			On Categories.CategoryID = Products.CategoryID
 	Group by CategoryName;
 
--- 21. Total customers per country/city
--- In the Customers table, show the total number of customers per Country and City.
+/*
+21. Total customers per country/city
+In the Customers table, show the total number of customers per Country and City.
+*/
 	Select 
 		Country
 		,City
@@ -32,12 +41,14 @@
 	Group by Country, City
 	Order by "Total Customers" desc;
 
--- 22. Products that need reordering
--- What products do we have in our inventory that should be reordered?
--- For now, just use the fields UnitsInStock and ReorderLevel, where
--- UnitsInStock is less than the ReorderLevel, ignoring the fields
--- UnitsOnOrder and Discontinued.
--- Order the results by ProductID.
+/*
+22. Products that need reordering
+What products do we have in our inventory that should be reordered?
+For now, just use the fields UnitsInStock and ReorderLevel, where
+UnitsInStock is less than the ReorderLevel, ignoring the fields
+UnitsOnOrder and Discontinued.
+Order the results by ProductID.
+*/
 	Select
 		ProductID		"Product ID"
 		,ProductName	"Product Name"
@@ -52,8 +63,8 @@
 Now we need to incorporate these fields—UnitsInStock, UnitsOnOrder,
 ReorderLevel, Discontinued—into our calculation. 
 We’ll define “products that need reordering” with the following:
--- UnitsInStock plus UnitsOnOrder are less than or equal to ReorderLevel
--- The Discontinued flag is false (0).
+	-- UnitsInStock plus UnitsOnOrder are less than or equal to ReorderLevel
+	-- The Discontinued flag is false (0).
 */
 	Select
 		ProductID		"Product ID"
@@ -72,8 +83,177 @@ We’ll define “products that need reordering” with the following:
 24. Customer list by region
 A salesperson for Northwind is going on a business trip to visit customers, 
 and would like to see a list of all customers, sorted by region, alphabetically.
-However, he wants the customers with no region (null in the Region
-field) to be at the end, instead of at the top, where you’d normally find
-the null values. Within the same region, companies should be sorted by
-CustomerID.
+
+However, he wants the customers with no region (null in the Region field) 
+to be at the end, instead of at the top, where you’d normally find the null values. 
+Within the same region, companies should be sorted by CustomerID.
 */
+	Select 
+		CustomerID		"Customer ID"
+		,CompanyName	"Company Name"
+		,Country
+		,region
+		,City
+		,Address
+		,Phone
+	From Customers
+	Order by 
+		-- order Region with null at the bottom
+		case
+			when Region is not null then 0
+			else 1
+		end asc,
+		Country,
+		CustomerID asc;
+		
+
+	Select 
+		CustomerID		"Customer ID"
+		,CompanyName	"Company Name"
+		,Country
+		,region
+		,City
+		,Address
+		,Phone
+	From Customers
+	Order by 
+		-- order Region with null at the bottom but not-null is desc.
+		Region desc,
+		Country,
+		CustomerID asc;
+
+
+/*
+25. High freight charges
+Some of the countries we ship to have very high freight charges. 
+We'd like to investigate some more shipping options for our customers, 
+to be able to offer them lower freight charges. 
+Return the three ship countries with the highest average freight overall, 
+in descending order by average freight.
+*/
+	Select --Top 3 
+		ShipCountry		'Ship Country'
+		,AVG(Freight)	'Average Freight'
+	From 
+		Orders
+	Group by ShipCountry
+	Order by 'Average Freight' desc;
+
+/*
+26. High freight charges - 1996 - modified
+We're continuing on the question above on high freight charges. 
+Now, instead of using all the orders we have, 
+we only want to see orders from the year 1996.
+*/
+	Select --Top 3 
+		ShipCountry		'Ship Country'
+		,AVG(Freight)	'Average Freight'
+	From 
+		Orders
+	Where Year(OrderDate) = '1996'
+	Group by ShipCountry
+	Order by 'Average Freight' desc;
+
+/*
+27. High freight charges with between - First Quater 2017 - modified
+*/
+	Select --Top 3 
+		ShipCountry		'Ship Country'
+		,AVG(Freight)	'Average Freight'
+	From 
+		Orders
+	Where Year(OrderDate) = '1997' and Month(OrderDate) between '01' and '03'
+	--Where Year(OrderDate) = '1997' and Month(OrderDate) >= '01' and Month(OrderDate) <= '03'
+	Group by ShipCountry
+	Order by 'Average Freight' desc;
+
+/*
+28. High freight charges - last 12 months of data - modified
+We're continuing to work on high freight charges. We now want to get
+the three ship countries with the highest average freight charges. 
+But instead of filtering for a particular year, we want to use the last 12
+months of order data, using as the end date the last OrderDate in Orders.
+Note: Range of date from a time can be used by DateAdd()
+*/
+	Select --Top 3 
+		ShipCountry		'Ship Country'
+		,AVG(Freight)	'Average Freight'
+	From 
+		Orders
+	Where OrderDate >= DateAdd(Month, -12, (select max(OrderDate) from Orders))
+	Group by ShipCountry
+	Order by 'Average Freight' desc;
+
+/*
+29. Inventory list
+We're doing inventory, and need to show information like the below, for all orders. 
+Sort by OrderID and Product ID.
+Note: 
+	-- EmployeeID from Employees
+	-- LastName from Employees
+	-- OrderID from Orders
+	-- ProductName from Products
+	-- Quantity from 'Order Detals'
+*/
+	Select 
+		Employees.EmployeeID	'Employee ID'
+		,Employees.LastName		'Last Name'
+		,Orders.OrderID			'Order ID'
+		,Products.ProductName	'Product Name'
+		,[Order Details].Quantity
+	From
+		Employees 
+		Join Orders On Employees.EmployeeID = Orders.EmployeeID
+		Join [Order Details] On [Order Details].OrderID = Orders.OrderID
+		Join Products On Products.ProductID = [Order Details].ProductID;
+
+/*
+30. Customers with no orders
+There are some customers who have never actually placed an order.
+Show these customers.
+Note: don't need to join as it make to code run slow: increase complexity.
+*/
+	Select 
+		*
+	From
+		Customers
+	Where Customers.CustomerID Not In (Select Orders.CustomerID from Orders);
+
+/*
+31. Customers with no orders for EmployeeID 4
+One employee (Margaret Peacock, EmployeeID 4) has placed the most orders. 
+However, there are some customers who've never placed an order with her. 
+Show only those customers who have never placed an order with her.
+*/
+	-- Customers having orders with EmployeeID 4
+	Select
+		Customers.ContactName
+	From 
+		Customers Left Join Orders
+			on Orders.CustomerID = Customers.CustomerID
+				and Orders.EmployeeID = 4;
+	
+	-- Customers not in the list of Customers having orders with EmployeeID 4
+	Select Distinct
+		Customers.CustomerID
+		,Customers.ContactName
+	From
+		Customers Left Join Orders 
+			On Customers.CustomerID = Orders.CustomerID
+	Where
+		Customers.ContactName NOT IN (
+			Select Customers.ContactName
+			From Customers
+				Lelf Join Orders 
+					On Orders.CustomerID = Customers.CustomerID AND Orders.EmployeeID = 4);
+
+	-- Alternately
+	Select 
+		Customers.CustomerID
+		,Customers.ContactName
+	From
+		Customers
+			Left Join Orders 
+				On Customers.CustomerID = Orders.CustomerID AND Orders.EmployeeID = 4
+	Where
+		Orders.CustomerID IS NULL;
--- Introductory Problems 1-19.sql
+++ Introductory Problems 1-19.sql
@@ -1,20 +1,29 @@
--- 1 - Which shippers do we have?
--- We have a table called Shippers. Return all the fields from all the shippers
+
+-- Introductory Problems 1 - 19
+
+/*
+1 - Which shippers do we have?
+We have a table called Shippers. Return all the fields from all the shippers
+*/
 
 	-- Simply select all
 	Select * from Shippers;
 
--- 2. Certain fields from Categories
--- Show two fields CategoryName and Description from Caterogies
+/*
+2. Certain fields from Categories
+Show two fields CategoryName and Description from Caterogies
+*/
 	Select 
 		CategoryName "Category Name",	-- rename column for display
 		Description
 	From Categories;
 
--- 3. Sales Representatives
--- We’d like to see just the FirstName, LastName, and HireDate of 
--- all the employees with the Title of Sales Representative. 
--- Extra condition: employed in or after 1993
+/*
+3. Sales Representatives
+We’d like to see just the FirstName, LastName, and HireDate of 
+all the employees with the Title of Sales Representative. 
+Extra condition: employed in or after 1993
+*/
 	Select 
 		FirstName	"First Name" 
 		,LastName	"Last Name"
@@ -22,9 +31,11 @@
 	From Employees
 	Where Title = 'Sales Representative' and Year(HireDate) >= 1993;
 
--- 4. Sales Representatives in the United States
--- Now we’d like to see the same columns as above, but only for those employees 
--- that both have the title of Sales Representative, and also are in the United States.
+/*
+4. Sales Representatives in the United States
+Now we’d like to see the same columns as above, but only for those employees 
+that both have the title of Sales Representative, and also are in the United States.
+*/
 	Select 
 		FirstName	"First Name" 
 		,LastName	"Last Name"
@@ -32,9 +43,11 @@
 	From Employees
 	Where Title = 'Sales Representative' and Country = 'USA';
 
--- 5. Orders placed by specific EmployeeID
--- Show all the orders placed by a specific employee. 
--- The EmployeeID for this Employee (Steven Buchanan) is 5.
+/*
+5. Orders placed by specific EmployeeID
+Show all the orders placed by a specific employee. 
+The EmployeeID for this Employee (Steven Buchanan) is 5.
+*/
 	Select 
 		OrderID		"Order ID"
 		,OrderDate	"Order Date"
@@ -42,9 +55,11 @@
 	From Orders
 	Where EmployeeID = 5;
 
--- 6. Suppliers and ContactTitles
--- In the Suppliers table, show the SupplierID, ContactName, and ContactTitle 
--- for those Suppliers whose ContactTitle is not Marketing Manager.
+/*
+6. Suppliers and ContactTitles
+In the Suppliers table, show the SupplierID, ContactName, and ContactTitle 
+for those Suppliers whose ContactTitle is not Marketing Manager.
+*/
 	Select 
 		SupplierID		"Supplier ID" 
 		,ContactName		"Contact Name" 
@@ -52,43 +67,47 @@
 	From Suppliers
 	Where ContactTitle != 'Marketing Manager';
 
--- 7. Products with “queso” in ProductName
--- In the products table, we’d like to see the ProductID and ProductName
--- for those products where the ProductName includes the string “queso”.
+/*
+7. Products with “queso” in ProductName
+In the products table, we’d like to see the ProductID and ProductName
+for those products where the ProductName includes the string “queso”.
+*/
 	Select 
 		ProductID		"Product ID"
 		,ProductName		"Product Name"
 	From Products
 	Where Charindex('queso', lower(ProductName)) > 0;
 
--- for those products where the ProductName starting with “Queso”.
--- Using wildcard 
+	-- for those products where the ProductName starting with “Queso”.
+	-- Using wildcard 
 	Select 
 		ProductID		"Product ID"
 		,ProductName		"Product Name"
 	From Products
 	Where ProductName Like 'Queso%';
 
--- for those products where the ProductName starting with “A - H”.
--- Using wildcard 
+	-- for those products where the ProductName starting with “A - H”.
+	-- Using wildcard 
 	Select 
 		ProductID		"Product ID"
 		,ProductName		"Product Name"
 	From Products
 	Where ProductName Like '[A-H]%';
 
--- for those products where the ProductName NOT starting with “A - H”.
--- Using wildcard 
+	-- for those products where the ProductName NOT starting with “A - H”.
+	-- Using wildcard 
 	Select 
 		ProductID		"Product ID"
 		,ProductName		"Product Name"
 	From Products
 	Where ProductName Like '[^A-H]%';
 
--- 8 - 9. Orders shipping to France or Belgium
--- Looking at the Orders table, there’s a field called ShipCountry. Write a
--- query that shows the OrderID, CustomerID, and ShipCountry for the
--- orders where the ShipCountry is either France or Belgium.
+/*
+8 - 9. Orders shipping to France or Belgium
+Looking at the Orders table, there’s a field called ShipCountry. Write a
+query that shows the OrderID, CustomerID, and ShipCountry for the
+orders where the ShipCountry is either France or Belgium.
+*/
 	Select 
 		OrderID		 "Order ID"
 		,CustomerID	 "Customer ID"
@@ -96,10 +115,12 @@
 	From Orders
 	Where ShipCountry In ('France', 'Belgium');
 
--- 10 - 11. Employees, in order of age
--- For all the employees in the Employees table, show the FirstName,
--- LastName, Title, and BirthDate. Order the results by BirthDate, so we
--- have the oldest employees first.
+/*
+10 - 11. Employees, in order of age
+For all the employees in the Employees table, show the FirstName,
+LastName, Title, and BirthDate. 
+Order the results by BirthDate, so we have the oldest employees first.
+*/
 	Select 
 		FirstName	"First Name" 
 		,LastName	"Last Name" 
@@ -109,11 +130,13 @@
 	From Employees
 	Order By BirthDate;
 
--- 12. Employees full name
--- Show the FirstName and LastName columns from the Employees table,
--- and then create a new column called FullName, showing FirstName and
--- LastName joined together in one column, with a space in-between.
--- Modified: show full name, title, and birth day
+/*
+12. Employees full name
+Show the FirstName and LastName columns from the Employees table,
+and then create a new column called FullName, showing FirstName and
+LastName joined together in one column, with a space in-between.
+Modified: show full name, title, and birth day
+*/
 	Select 
 		Concat(FirstName, ' ', LastName)	"Full Name" 
 		,Title,
@@ -121,12 +144,14 @@
 		Convert(date, BirthDate) "Birth Date"
 	From Employees;
 
--- 13. OrderDetails amount per line item
--- In the OrderDetails table, we have the fields UnitPrice and Quantity.
--- Create a new field, TotalPrice, that multiplies these two together. 
--- In addition, show the OrderID, ProductID, UnitPrice, and Quantity.
--- Order by OrderID and ProductID.
--- Modify: Here Discount is also applied
+/*
+13. OrderDetails amount per line item
+In the OrderDetails table, we have the fields UnitPrice and Quantity.
+Create a new field, TotalPrice, that multiplies these two together. 
+In addition, show the OrderID, ProductID, UnitPrice, and Quantity.
+Order by OrderID and ProductID.
+Modify: Here Discount is also applied
+*/
 	Select
 		OrderID		"Order ID"
 		,ProductID	"Product ID"
@@ -137,16 +162,20 @@
 	From "Order Details"
 	Order By OrderID, ProductID;
 
--- 14. How many customers?
--- How many customers do we have in the Customers table? Show one value only, 
--- and don’t rely on getting the recordcount at the end of a resultset.
+/*
+14. How many customers?
+How many customers do we have in the Customers table? Show one value only, 
+and don’t rely on getting the recordcount at the end of a resultset.
+*/
 	Select
 		count(CustomerID) "Number of Customers"
 	From 
 		Customers;
 
--- 15. When was the first order?
--- Show the date of the first order ever made in the Orders table.
+/*
+15. When was the first order?
+Show the date of the first order ever made in the Orders table.
+*/
 	
 	-- Assumption: orders were entered by ordered dates
 	Select top 1 
@@ -159,10 +188,11 @@
 		Convert(date, min(OrderDate))
 	From Orders;
 		
-
--- 16. Countries where there are customers
--- Show a list of countries where the Northwind company has customers.
--- There are several ways for this.
+/*
+16. Countries where there are customers
+Show a list of countries where the Northwind company has customers.
+There are several ways for this.
+*/
 	-- Use Orders table
 	Select Distinct
 		ShipCountry "Customer Countries"
@@ -175,11 +205,13 @@
 	From 
 		Customers;
 		
--- 17. Contact titles for customers
--- Show a list of all the different values in the Customers table for
--- ContactTitles. Also include a count for each ContactTitle.
--- This is similar in concept to the previous question “Countries where
--- there are customers”, except we now want a count for each ContactTitle.
+/*
+17. Contact titles for customers
+Show a list of all the different values in the Customers table for
+ContactTitles. Also include a count for each ContactTitle.
+This is similar in concept to the previous question “Countries where
+there are customers”, except we now want a count for each ContactTitle.
+*/
 	Select 
 		ContactTitle		"Contact Titles"
 		,count(ContactTitle) "Count"
@@ -187,10 +219,12 @@
 		Customers
 	Group By ContactTitle;
 
--- 18. Products with associated supplier names
--- We’d like to show, for each product, the associated Supplier. 
--- Show the ProductID, ProductName, and the CompanyName of the Supplier. 
--- Sort by ProductID.
+/*
+18. Products with associated supplier names
+We’d like to show, for each product, the associated Supplier. 
+Show the ProductID, ProductName, and the CompanyName of the Supplier. 
+Sort by ProductID.
+*/
 	Select 
 		p.ProductID		"Product ID"
 		,p.ProductName	"Product Name"
